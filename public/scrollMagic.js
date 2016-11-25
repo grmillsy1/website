ba84209884d031b95ec47fdf49e3956871cabe4d
Georgia Mills <georgee_123@hotmail.com>
2016-11-25 17:22:23 +0000

    added pinned feature

--- public/scrollMagic.js
+++ public/scrollMagic.js
@@ -1,41 +1,59 @@
 
-$(function() {
 
-var controller = new ScrollMagic.Controller({globalSceneOptions: {duration: '100%'}});
+// $(function() {
+// if ($( window ).width() >= 604) {
+
+var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: 0.05,}});
 
 // build scenes
- new ScrollMagic.Scene({triggerElement: "#sec1"})
-        .setClassToggle("#menu1", "active") // add class toggle
-        .addIndicators()
-        .addTo(controller);
-new ScrollMagic.Scene({triggerElement: "#sec2"})
-        .setClassToggle("#menu2", "active") // add class toggle
-        .addIndicators()
-        .addTo(controller);
-new ScrollMagic.Scene({triggerElement: "#sec3"})
-        .setClassToggle("#menu3", "active") // add class toggle
-        .addIndicators()
-        .addTo(controller);
-new ScrollMagic.Scene({triggerElement: "#sec4"})
-        .setClassToggle("#menu4", "active") // add class toggle
-        .addIndicators()
-        .addTo(controller);
-new ScrollMagic.Scene({triggerElement: "#sec5"})
-        .setClassToggle("#menu5", "active") // add class toggle
-        .addIndicators()
-        .addTo(controller);
-new ScrollMagic.Scene({triggerElement: "#sec6"})
-        .setClassToggle("#menu6", "active") // add class toggle
-        .addIndicators()
-        .addTo(controller);
-)}
+ var scene1 = new ScrollMagic.Scene({
+   duration: '100%',
+   triggerElement: "#sec1"})
+      .setClassToggle("#menu1", "active") // add class toggle
+      .addIndicators()
+      .addTo(controller);
 
-$(function() {
-  var controller2 = new ScrollMagic.Controller();
+  var scene1b = new ScrollMagic.Scene({
+    triggerElement: "#photo1",
+    duration: 350})
+       .setPin('#sec1-text')
+       .addIndicators()
+       .addTo(controller);
 
-  var scene1 = new ScrollMagic.Scene({
-    triggerElement: ''
-  })
+var scene2 = new ScrollMagic.Scene({
+  triggerElement: "#sec2",
+  duration: '100%',})
+    .setClassToggle("#menu2", "active") // add class toggle
+    .addIndicators()
+    .addTo(controller);
 
+var scene3 = new ScrollMagic.Scene({
+  triggerElement: "#sec3",
+  duration: '100%'})
+    .setClassToggle("#menu3", "active") // add class toggle
+    .addIndicators()
+    .addTo(controller);
 
-})
+var scene4 = new ScrollMagic.Scene({
+  duration: '100%',
+  triggerElement: "#sec4"})
+    .setClassToggle("#menu4", "active") // add class toggle
+    .addIndicators()
+    .addTo(controller);
+
+var scene5 = new ScrollMagic.Scene({
+  duration: '100%',
+  triggerElement: "#sec5"})
+    .setClassToggle("#menu5", "active") // add class toggle
+    .addIndicators()
+    .addTo(controller);
+
+var scene5 = new ScrollMagic.Scene({
+  duration: '100%',
+  triggerElement: "#sec6"})
+    .setClassToggle("#menu6", "active") // add class toggle
+    .addIndicators()
+    .addTo(controller);
+
+// )}
+// }
