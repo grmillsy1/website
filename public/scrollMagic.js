4e7eac055fd77e1ea0554770d4a62216a734f357
Georgia Mills <georgee_123@hotmail.com>
2016-11-30 20:22:43 +0000

    changed writing

--- public/scrollMagic.js
+++ public/scrollMagic.js
@@ -5,11 +5,11 @@
 
 var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: 0.05,}});
 
-// build scenes
  var scene1 = new ScrollMagic.Scene({
    duration: '100%',
    triggerElement: "#sec1"})
-      .setClassToggle("#menu1", "active") // add class toggle
+      .setClassToggle("#menu1", "active")
+      .addIndicators()
       .addTo(controller);
 
   var scene1b = new ScrollMagic.Scene({
@@ -22,22 +22,26 @@ var scene2 = new ScrollMagic.Scene({
   triggerElement: "#sec2",
   offset: -100,
   duration: '80%',})
-    .setClassToggle("#menu2", "active") // add class toggle
+    .setClassToggle("#menu2", "active")
+    .addIndicators()
     .addTo(controller);
+
   var scene2b = new ScrollMagic.Scene({
     triggerElement: "#sec2",
     offset: -150,
     // duration: 200,
     })
      .setTween('#sec2-line', 0.5, {scale: 1.5})
-     .addIndicators()
      .addTo(controller);
 
 var scene3 = new ScrollMagic.Scene({
   triggerElement: "#sec3",
-  duration: '200%'})
-    .setClassToggle("#menu3", "active") // add class toggle
+  duration: '200%',
+  offset: -100 })
+    .setClassToggle("#menu3", "active")
+    .addIndicators()
     .addTo(controller);
+
   var scene3b = new ScrollMagic.Scene({
     triggerElement: "#photo3",
     duration: 350})
@@ -46,9 +50,12 @@ var scene3 = new ScrollMagic.Scene({
 
 var scene4 = new ScrollMagic.Scene({
   duration: '100%',
-  triggerElement: "#sec4"})
-    .setClassToggle("#menu4", "active") // add class toggle
+  triggerElement: "#sec4",
+  offset: -100 })
+    .setClassToggle("#menu4", "active")
+    .addIndicators()
     .addTo(controller);
+
   var scene4b = new ScrollMagic.Scene({
     triggerElement: "#photo4",
     duration: 350})
@@ -57,9 +64,12 @@ var scene4 = new ScrollMagic.Scene({
 
 var scene5 = new ScrollMagic.Scene({
   duration: '200%',
-  triggerElement: "#sec5"})
-    .setClassToggle("#menu5", "active") // add class toggle
+  triggerElement: "#sec5",
+   offset: -100 })
+    .setClassToggle("#menu5", "active")
+    .addIndicators()
     .addTo(controller);
+
   var scene5b = new ScrollMagic.Scene({
     triggerElement: "#photo5",
     duration: 350})
@@ -68,9 +78,12 @@ var scene5 = new ScrollMagic.Scene({
 
 var scene6 = new ScrollMagic.Scene({
   duration: '100%',
-  triggerElement: "#sec6"})
-    .setClassToggle("#menu6", "active") // add class toggle
+  triggerElement: "#sec6",
+  offset: -100 })
+    .setClassToggle("#menu6", "active")
+    .addIndicators()
     .addTo(controller);
+
   var scene6b = new ScrollMagic.Scene({
     triggerElement: "#photo6",
     duration: 350})
@@ -79,8 +92,10 @@ var scene6 = new ScrollMagic.Scene({
 
 var scene7 = new ScrollMagic.Scene({
   duration: '100%',
-  triggerElement: "#sec7"})
-    .setClassToggle("#menu7", "active") // add class toggle
+  triggerElement: "#sec7",
+  offset: -100 })
+    .setClassToggle("#menu7", "active")
+    .addIndicators()
     .addTo(controller);
 
 // )}
